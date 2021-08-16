d3e1a8edd953744cb47a99a5c05422a2f3785d3c
035WasTaken <60830475+035WasTaken@users.noreply.github.com>
2021-08-15 21:31:19 -0400

    removed start.bat as it is no longer relevant

--- server/start.bat
+++
@@ -1,5 +0,0 @@
-@ECHO off
-REM tsc & node server/index.server.js
-REM use above comment instead of below line if you dont have a fucked project structure
-tsc & node javarock-site/server/index.server.js
-pause
