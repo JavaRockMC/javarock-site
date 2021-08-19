d0df17268f5fe6425eb1b1bd82ac2b2632c07a50
035WasTaken <60830475+035WasTaken@users.noreply.github.com>
2021-08-19 01:03:04 +0000

    changes

---
+++ src/public/js/contact.js
@@ -0,0 +1,10 @@
+const xhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP")
+
+const textBox = document.getElementById("textbox");
+const submitButton = document.querySelector("button");
+
+textBox.addEventListener("input", (value) => {
+    if(!value) {
+        
+    }
+})
