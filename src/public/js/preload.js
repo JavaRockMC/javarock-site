ac689b3a143502fed201a0ece7588a80b967c221
DUDEbehindDUDE <66212375+DUDEbehindDUDE@users.noreply.github.com>
2021-08-26 09:47:55 -0400

    add some more styling and add .css.map to gitignore

--- src/public/js/preload.js
+++ src/public/js/preload.js
@@ -1,3 +1,3 @@
-//removes all classes (including the preload class) from nav bar img when this loads
+//removes all classes (mainly to remove the preload class) from nav bar img when this loads
 var obj = document.getElementById("navimg");
 obj.removeAttribute("class");
